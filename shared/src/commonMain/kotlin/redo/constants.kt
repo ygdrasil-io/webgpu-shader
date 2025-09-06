160abbf19b3c72abd664cb35532fefb2b4cfc0b0
Alexandre Mommers <alexandre.mommers@gmail.com>
2025-09-06 18:33:48 +0200

    refactor: add `i32` and `bool` functions to `ShaderBuilderScope` for extended constant handling

--- shared/src/commonMain/kotlin/redo/constants.kt
+++ shared/src/commonMain/kotlin/redo/constants.kt
@@ -5,6 +5,17 @@ fun ShaderBuilderScope.f32(value: Double): f32 {
     return getDefaultValue()
 }
 
+fun ShaderBuilderScope.i32(value: Int): f32 {
+    ConstantStatement(this, value.toString())
+    return getDefaultValue()
+}
+
+fun ShaderBuilderScope.bool(value: Boolean): f32 {
+    ConstantStatement(this, value.toString())
+    return getDefaultValue()
+}
+
+
 internal class ConstantStatement(
     scope: ShaderBuilderScope,
     val value: String
