7e6a59149a9aadaf451c30bc8a560ae4d01f9474
ygdrasil-io <alexandre.mommers@gmail.com>
2025-12-02 02:55:56 +0100

    **feat:** Add configurable output directory to `CodeGeneratorTask` with default convention

--- buildSrc/src/main/kotlin/CodeGeneratorTask.kt
+++ buildSrc/src/main/kotlin/CodeGeneratorTask.kt
@@ -3,24 +3,30 @@ import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeSpec
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import java.io.File
 
 open class CodeGeneratorTask : DefaultTask() {
 
+    @OutputDirectory
+    val outputDirectory: DirectoryProperty = project.objects.directoryProperty()
+
     init {
         group = "generator"
+        outputDirectory.convention(
+            project.layout.projectDirectory
+                .dir("shared")
+                .dir("src")
+                .dir("commonMain")
+                .dir("kotlin")
+        )
     }
 
     @TaskAction
     fun generate() {
-        generate(
-            project.projectDir
-                .resolve("shared")
-                .resolve("src")
-                .resolve("commonMain")
-                .resolve("kotlin")
-        )
+        generate(outputDirectory.get().asFile)
     }
 
     fun generate(outputDir: File) {
